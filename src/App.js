46bf596a3cafd7b980d969be19a31f37ca10d8db
TabishGenesis <tabish@genesis360.co>
2023-12-17 06:45:51 +0500

    Changes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import "./App.css";
 import { useWalletConnectModal } from "my-walletapp";
 
 function App() {
-  const { openModal, closeConnection } = useWalletConnectModal();
+  const { openModal, closeConnection, sendTransaction } =
+    useWalletConnectModal();
+
+  const sendDummyTransaction = async () => {
+    const tx = {
+      chainId: 1,
+      method: "sendTransaction",
+      data: "fdlksfns",
+    };
+    await sendTransaction(tx);
+    console.log("Dummy transaction sent");
+  };
   return (
     <div className="App">
       <button onClick={openModal}>Connect Wallet</button>
       <button onClick={closeConnection}>Disconnect Wallet</button>
+      <button onClick={sendDummyTransaction}>Send Dummy Transaction</button>
     </div>
   );
 }
