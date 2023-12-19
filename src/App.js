03c20fd74d9c41013ea5369e401ee3658ef63b45
TabishGenesis <tabish@genesis360.co>
2023-12-19 05:18:49 +0500

    Updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from "react";
 import "./App.css";
-import { useWalletConnectModal } from "my-walletapp";
+import { usePlenaWallet } from "plena-wallet-sdk";
 
 function App() {
-  const { openModal, closeConnection, sendTransaction } =
-    useWalletConnectModal();
+  const { openModal, closeConnection, sendTransaction, walletAddress } =
+    usePlenaWallet();
 
   const sendDummyTransaction = async () => {
     const tx = {
       chainId: 1,
-      method: "sendTransaction",
+      method: "send_transaction",
       data: "fdlksfns",
     };
     await sendTransaction(tx);
     console.log("Dummy transaction sent");
+  };
+
+  const printWalletAddress = () => {
+    console.log("App Wallet Address: ", walletAddress);
   };
   return (
     <div className="App">
       <button onClick={openModal}>Connect Wallet</button>
       <button onClick={closeConnection}>Disconnect Wallet</button>
       <button onClick={sendDummyTransaction}>Send Dummy Transaction</button>
+      <button onClick={printWalletAddress}>GetWalletAddress</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { WalletConnectProvider } from "my-walletapp";
+import { PlenaWalletProvider } from "plena-wallet-sdk";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const config = {
+  dappToken:
+    "269a0d4e4b338001c220588534ae809bfc6b1a322415f2561e93e08d5446bb352b08d5a2d85b9fac75ad15d72a087c9a95857e4e265ce0e368440733e9a130b9",
+  dappId: "clotfuhlkqlqs7on7o0g",
+  bridgeUrl: "connect.plena.finance",
+};
 root.render(
-  <WalletConnectProvider>
+  <PlenaWalletProvider config={config}>
     <App />
-  </WalletConnectProvider>
+  </PlenaWalletProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
